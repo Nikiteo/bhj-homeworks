1427f3f6960c92c3d9ed6c9d550c9494ea15b818
Nikiteo <nzhigiley@yandex.ru>
2020-08-10 15:32:14 +0300

    first

---
+++ element-search/menu/task.js
@@ -0,0 +1,19 @@
+const links = Array.from(document.getElementsByClassName('menu__link'));
+
+function clicks(e) {
+    if (this.nextElementSibling) {
+        e.preventDefault();
+        const active = document.querySelector('.menu_active');
+        if (active) {
+            active.classList.remove('menu_active')
+            if (e.target === active.previousElementSibling) {
+                return;
+            }
+        }
+        this.nextElementSibling.classList.add('menu_active')
+    }
+}
+
+for (i = 0; i < links.length; i++) {
+    links[i].addEventListener('click', clicks);
+}
